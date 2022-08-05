1113bd808b842801482e8a340c03005d74f37896
Hazey <hassaanhmd@gmail.com>
2022-08-05 13:57:03 +0200

    Used react-redux to track favorited meals =====> App Complete (Section 7 Complete)

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,15 +1,17 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import MealsList from '../components/MealsList/MealsList';
 import { MEALS } from '../data/dummy-data';
-import { FavoritesContext } from '../store/context/favorites-context';
+// import { FavoritesContext } from '../store/context/favorites-context';
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  //   const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
 
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
   );
 
   if (favoriteMeals.length === 0) {
@@ -26,14 +28,14 @@ function FavoritesScreen() {
 export default FavoritesScreen;
 
 const styles = StyleSheet.create({
-    rootContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: 'white',
-    }
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
